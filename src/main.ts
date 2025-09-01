1edce967fdbdb792175d5694b48e96c71e3fcc8f
Basanta <basantaparajuli.11@gmail.com>
2025-09-01 12:42:52 +0545

    initializing inceptors and logger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/http-exception.filter';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,8 +14,8 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  // app.useGlobalFilters(new HttpExceptionFilter());
-  // app.useGlobalInterceptors(new LoggingInceptor())
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new LoggingInterceptor());
   await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000);
 }
 bootstrap();
