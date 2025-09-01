36d7a443f99d0d6569d66801160fbd5e6698e78f
Basanta <basantaparajuli.11@gmail.com>
2025-09-01 14:26:17 +0545

    ssd

--- src/invoices/invoices.controller.ts
+++ src/invoices/invoices.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { InvoicesService } from './invoices.service';
+import { CreateInvoiceDto } from './dto/create-invoice.dto';
 
 @Controller('invoices')
-export class InvoicesController {}
+export class InvoicesController {
+  constructor(private readonly invoicesService: InvoicesService) {}
+
+  @Post()
+  async create(@Body() dto: CreateInvoiceDto) {
+    return this.invoicesService.create(dto);
+  }
+
+  @Get()
+  async findAll() {
+    return this.invoicesService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.invoicesService.findOne(id);
+  }
+
+  @Patch(':id/pay')
+  async markPaid(@Param('id') id: string) {
+    return this.invoicesService.markPaid(id);
+  }
+
+  @Patch(':id/cancel')
+  async cancel(@Param('id') id: string) {
+    return this.invoicesService.cancel(id);
+  }
+}
--- src/invoices/invoices.module.ts
+++ src/invoices/invoices.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { InvoicesService } from './invoices.service';
 import { InvoicesController } from './invoices.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Invoice } from 'src/entities/invoice.entity';
+import { InvoiceItem } from 'src/entities/invoice-item.entity';
+import { Customer } from 'src/entities/customer.entity';
+import { Product } from 'src/entities/product.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Invoice, InvoiceItem, Customer, Product]),
+  ],
   providers: [InvoicesService],
-  controllers: [InvoicesController]
+  controllers: [InvoicesController],
 })
 export class InvoicesModule {}
